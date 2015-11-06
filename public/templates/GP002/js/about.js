a5713d3d342763d4d8cbeb23c9c0ce6ba7b03c54
E <01@eexx.me>
2015-11-06 10:32:45 +0800

    忽略模板

--- public/templates/GP002/js/about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	var page_name;
-	$('.containters .menu .nav li').each(function(){
-		if($(this).hasClass('selected')){
-			page_name = $(this).find('a').text();
-		}
-	});
-	$('.page_name').text(page_name);
-});
