a64be91fd474a82abc3ba5fcfbbede260052a2d5
pdc <pengdingchao@xm12t.com>
2015-11-16 17:58:08 +0800

    测试3

--- app/controllers/HtmlController.php
+++ app/controllers/HtmlController.php
@@ -309,7 +309,7 @@ class HtmlController extends BaseController{
                 ftp_login($conn,$customerinfo->ftp_user,$customerinfo->ftp_pwd);
                 ftp_pasv($conn, 1);
                 ftp_put($conn,$this->customer."/site.zip",$path,FTP_BINARY);
-                dd（$conn）;
+                dd($conn);
                 ftp_put($conn,$this->customer."/unzip.php",public_path("packages/unzip.php"),FTP_ASCII);
                 ftp_put($conn,$this->customer."/quickbar.json",public_path('customers/'.$this->customer.'/quickbar.json'),FTP_ASCII);
                 ftp_put($conn,$this->customer."/mobile/quickbar.json",public_path('customers/'.$this->customer.'/quickbar.json'),FTP_ASCII);
