b2e801a8336286ea47c37c5460667c36aa7c904d
miaolw <miaolinwei@xm12t.com>
2015-11-17 09:13:26 +0800

    修改冲突

--- public/admin/js/commenfun.js
+++ public/admin/js/commenfun.js
@@ -32,15 +32,7 @@ function tanchuang(ColumnInit){
 		});
         $('#bomb-box').addClass('in');
         clear_info();
-<<<<<<< HEAD
-        Save_id(1);
-=======
-<<<<<<< HEAD
-        Save_id(1);
-=======
         ColumnInit._Save_id(1);
->>>>>>> 07ec9de051c87377bce4ef8777ee9784a4333ca5
->>>>>>> develop
     });
     //数据清除
     function clear_info(){
