65f819061b883ed466a3a70ab311cc85721ba994
pdc <pengdingchao@xm12t.com>
2015-11-17 17:26:23 +0800

    回归主线

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -27,10 +27,7 @@ class ArticleController extends BaseController{
         }
         $cus_id=Auth::id();
         $article->title=trim(Input::get('title'));
-        
-        if(strlen(Input::get('c_id'))!=0){
-        $article->c_id=Input::get('c_id');}
-        else{$article->c_id='0';}
+        $article->c_id=Input::get('c_id');
         $article->viewcount=Input::get('viewcount')?Input::get('viewcount'):0;
         $article->title_bold=Input::get('title_bold');
         $article->title_color=Input::get('title_color');
@@ -61,7 +58,6 @@ class ArticleController extends BaseController{
         }     
         $article->cus_id=$cus_id;
         $result=$article->save();
-        dd($result);
         if($result){
             if($id){
                 MoreImg::where('a_id',$id)->delete();
