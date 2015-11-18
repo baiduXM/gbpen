d27559af19b397d96615ec06c470ae693800388b
pdc <pengdingchao@xm12t.com>
2015-11-18 17:15:05 +0800

    ceshi

--- app/controllers/HtmlController.php
+++ app/controllers/HtmlController.php
@@ -308,8 +308,8 @@ class HtmlController extends BaseController{
             if($conn){
                 ftp_login($conn,$customerinfo->ftp_user,$customerinfo->ftp_pwd);
                 ftp_pasv($conn, 1);
-                //dd($conn,$this->customer,$path);
-                ftp_put($conn,$this->customer."/site.zip",$path,FTP_BINARY);
+               // dd($conn,$this->customer,$path);
+                ftp_put("/httpdocs/".$conn,$this->customer."/site.zip",$path,FTP_BINARY);
                 
                 ftp_put($conn,$this->customer."/unzip.php",public_path("packages/unzip.php"),FTP_ASCII);
                 ftp_put($conn,$this->customer."/quickbar.json",public_path('customers/'.$this->customer.'/quickbar.json'),FTP_ASCII);
