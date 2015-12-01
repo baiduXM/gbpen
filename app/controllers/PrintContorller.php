3648d7e0bdf0838ded5f311b1af29b393fbd3d97
pdc <pengdingchao@xm12t.com>
2015-12-01 13:49:01 +0800

    打印config.ini的type类型

--- app/controllers/PrintContorller.php
+++ app/controllers/PrintContorller.php
@@ -486,7 +486,8 @@ class PrintController extends BaseController{
     public function quickBarJson(){
         $config_str=file_get_contents(public_path('/templates/'.$this->themename).'/config.ini');
         $search="/QuickBar=(.*)/i";
-        dd($config_str);
+        $searchtype="/Type=(.*)/i";
+        dd($searchtype);
         $result=preg_match($search,$config_str,$config_arr);
         if($result!=0){
             if (trim($config_arr[1])!="custom") {
