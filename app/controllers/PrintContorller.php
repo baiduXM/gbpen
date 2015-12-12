f5bd8c2907f47b32717d31bc169663255f52c537
pdc <pengdingchao@xm12t.com>
2015-12-12 10:14:11 +0800

    推送错误测试

--- app/controllers/PrintContorller.php
+++ app/controllers/PrintContorller.php
@@ -1802,7 +1802,6 @@ class PrintController extends BaseController{
                 $pagenavs = [];
             }
         }
-        dd($result);
         $result['pagenavs']=$pagenavs;
         $result['posnavs']=$this->getPosNavs($article->c_id);
         $result['title'] = $article->title;
