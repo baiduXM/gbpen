ca1edd3abc62a3890f1562b384ed17816953d535
miaolw <miaolinwei@xm12t.com>
2016-01-04 15:28:05 +0800

    no message

--- app/controllers/PrintContorller.php
+++ app/controllers/PrintContorller.php
@@ -1491,7 +1491,7 @@ class PrintController extends BaseController{
             $s.="delete _bd_share_config.image\n";
         }
 		$s.= "window.onload=function(){with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];}\n";
-		$s.="参数：".$params['sharePic']."</script>";
+		$s.="console.log(".$params['sharePic'].")</script>";
         echo $s;
 	}
        
