e7a574a97cc5cf32062e632d121e2f03f4e74bd7
pdc <pengdingchao@xm12t.com>
2016-02-02 14:31:00 +0800

    修复token

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -11,7 +11,7 @@ class ApiController extends BaseController{
         $timemap = Input::get('timemap');
         $data=md5(md5($timemap));
         $url = Config::get('url.DL_domain');
-    	$token = file_get_contents($url.'/?module=ApiModel&action=GetHandShake&num='.$data);
+    	$token = file_get_contents('http://dl.5067.org/?module=ApiModel&action=GetHandShake&num='.$data);
     	$taget = Input::get('taget');
     	$string = $token.$data;
     	if(md5($string)==$taget){
