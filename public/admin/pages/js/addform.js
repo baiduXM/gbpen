5d9ad1b86c7eb7090346dc244484ab0589948978
BoomThanks <1195015834@qq.com>
2016-05-24 17:32:35 +0800

    form

--- public/admin/pages/js/addform.js
+++ public/admin/pages/js/addform.js
@@ -69,8 +69,8 @@ function addformController($scope, $http, $location) {
      */
     function getFormElement() {
         $http.get('../form-element-list').success(function (json) {
-            console.log(json);
-            console.log('---/form-element-list---');
+//            console.log(json);
+//            console.log('---/form-element-list---');
             checkJSON(json, function (json) {
                 var _data = json.data;
                 if (_data != null) {
@@ -87,12 +87,13 @@ function addformController($scope, $http, $location) {
      */
     function getFormColumn() {
         $.get('../form-column-list', {form_id: form_id}, function (json) {
-            console.log(json);
-            console.log('---/form-column-list---');
+//            console.log(json);
+//            console.log('---/form-column-list---');
             checkJSON(json, function (json) {
                 var _data = json.data;
                 if (_data != null) {
                     $.each(_data, function (k, v) {
+                        console.log('===show_' + v.type + '===');
                         _html('show', v);
                     });
                 }
@@ -200,10 +201,10 @@ function addformController($scope, $http, $location) {
         });
         $('#element-box').html(_div);
         //===绑定动作===
-//        $('.unit-list>li').click(function () {
-//            var _this = $(this);
-//            bindElementEvent(_this);
-//        });
+        $('.unit-list>li').click(function () {
+            var _this = $(this);
+            bindElementEvent(_this);
+        });
     }
     /**
      * 绑定组件事件
@@ -219,10 +220,12 @@ function addformController($scope, $http, $location) {
             $('#item_2').addClass('tab-head-item-active');
             $('.tab-content-item').hide();
             $('#con_item_2').show();
+
             _html('show', _data);
-            _html('edit', _data);
-            _div_show(_data);
-            _div_edit(_data);
+
+//            _html('edit', _data);
+//            _div_show(_data);
+//            _div_edit(_data);
         });
     }
 
@@ -234,23 +237,48 @@ function addformController($scope, $http, $location) {
      * @param {string} operate 操作类型：show, edit, update
      * @param {array} data 传入数据
      * _show_{type} 显示/更新表单组件
+     * 绑定点击编辑动作
+     * 添加排序移动功能
      * _edit_{type} 编辑表单组件
+     * 
      * type={text, textarea, radio, checkbox, select, date, address, image, file}
      * @returns {undefined}
      */
     function _html(operate, data) {
         var _operate = operate;
         var _data = data;
-        var _config = _data.config;
-        var _temp = _config.option_default;
+//        var _config = _data.config;
+//        var _temp = _config.option_default;
         var _div = '';
         //显示布局数据
         //设置默认值
         if (_operate == 'show') {
-            //   var _div = '<li class="list-item" data-type="' + data.type + '" data-id="' + data.column_id + '" data-order="' + data.order + '">';
-            _div += eval('_' + _operate + '_' + _data.type + '(_data)');
-//            _div += '</li>';
+            _div += eval('_' + _operate + '_' + _data.type + '(_data)');//调用方法
             $('.element-show').append(_div);
+//            <li>
+//                <p>标题*描述</p>
+//                <input type="text"/>各类型input
+//            </li>
+
+            //===绑定元素点击响应事件===
+            $(".element-show>li").unbind('click').on('click', function () {
+                $('.tab-head-item').removeClass('tab-head-item-active');
+                $('#item_2').addClass('tab-head-item-active');
+                $('.tab-content-item').hide();
+                $('#con_item_2').show();
+                var _this_column = $(this);
+                _this_column.siblings().removeClass("click");
+                _this_column.addClass("click");
+                $.get('../form-column', {form_id: form_id, column_id: _this_column.attr('data-id')}, function (json) {
+                    _div_edit(json.data);
+                }, 'json');
+            });
+            //===绑定元素鼠标滑动事件===
+            $(".element-show>li").unbind('hover').hover(function () {
+                $(this).addClass("hover");
+            }, function () {
+                $(this).removeClass("hover");
+            });
         }
         if (_operate == 'edit') {
             alert(1)
@@ -259,22 +287,24 @@ function addformController($scope, $http, $location) {
 
     function _show_text(data) {
         var _data = data;
+
+
+
+
         var _div = '<li class="list-item" data-type="' + _data.type + '" data-id="' + _data.column_id + '" data-order="' + _data.order + '">';
         _div += '<p class="content-l">' + _data.title;
         if (_data.required == 1) {
             _div += '<i class="red-asterisk">*</i>';
         }
         _div += '</p>';
-        if (_data.description == null || _data.description == '') {
+        if (_data.description == null && _data.description == '') {
+            _div += '<i class="content-d">' + data.description + '</i>';
         }
         _div += '<input type="text" value="" disabled="disabled" placeholder="' + _data.description + '"/>';// name="col_' + _data.column_id + '"
         _div += '</li>';
         return _div;
     }
-    function _edit_text(data) {
-        var _data = data;
-        var _config = _data.config;
-    }
+
 
     function _show_textarea(data) {
         var _data = data;
@@ -284,9 +314,7 @@ function addformController($scope, $http, $location) {
         _div += '</li>';
         return _div;
     }
-    function _edit_textarea(data) {
-        var _data = data;
-    }
+
 
     function _show_radio(data) {
         var _data = data;
@@ -299,8 +327,8 @@ function addformController($scope, $http, $location) {
         if (_data.required == 1) {
             _div += '<i class="red-asterisk">*</i>';
         }
-        if (_data.description != null || _data.description != '') {
-            _div += '(' + data.description + ')';
+        if (_data.description != null && _data.description != '') {
+            _div += '<i class="content-d">' + data.description + '</i>';
         }
         _div += '</p>';
         $.each(_option_key, function (tk, tv) {
@@ -317,9 +345,7 @@ function addformController($scope, $http, $location) {
         _div += '</li>';
         return _div;
     }
-    function _edit_radio(data) {
-        var _data = data;
-    }
+
     function _show_checkbox(data) {
         var _data = data;
         var _config = _data.config;
@@ -351,9 +377,7 @@ function addformController($scope, $http, $location) {
         _div += '</li>';
         return _div;
     }
-    function _edit_checkbox(data) {
-        var _data = data;
-    }
+
     function _show_select(data) {
         var _data = data;
         var _config = _data.config;
@@ -368,43 +392,60 @@ function addformController($scope, $http, $location) {
         if (_data.description != null || _data.description != '') {
             _div += '(' + data.description + ')';
         }
-        _div += '</p>';        
+        _div += '</p>';
         _div += '<select name="col_' + data.column_id + '" disabled="disabled">';
         $.each(_option_key, function (tk, tv) {
             _to = "option_" + tv;
-            _div += '<option value=' + tv + '>' + _config[to] + '</option>';
+            _div += '<option value=' + tv + '>' + _config[_to] + '</option>';
         });
         _div += '</select>';
     }
-    function _edit_select(data) {
-        var _data = data;
-    }
+
     function _show_date(data) {
         var _data = data;
         _div += '<label class="content-l">' + data.title + '(' + data.description + ')：</label>';
         _div += '<input  type="text" name="col_' + data.column_id + '" value=""  disabled="disabled" />';
     }
-    function _edit_date(data) {
-        var _data = data;
-    }
+
     function _show_address(data) {
         var _data = data;
         _div += '<label class="content-l">' + data.title + '(' + data.description + ')：</label>';
         _div += '<input  type="text" name="col_' + data.column_id + '" value=""  disabled="disabled" />';
     }
-    function _edit_address(data) {
-        var _data = data;
-    }
+
     function _show_image(data) {
         var _data = data;
         alert('_show_image');
     }
-    function _edit_image(data) {
-        var _data = data;
-    }
+
     function _show_file(data) {
         var _data = data;
         alert(_data.text);
+    }
+    function _edit_text(data) {
+        var _data = data;
+        var _config = _data.config;
+    }
+    function _edit_textarea(data) {
+        var _data = data;
+    }
+    function _edit_radio(data) {
+        var _data = data;
+    }
+    function _edit_checkbox(data) {
+        var _data = data;
+    }
+    function _edit_select(data) {
+        var _data = data;
+    }
+    function _edit_date(data) {
+        var _data = data;
+    }
+    function _edit_address(data) {
+        var _data = data;
+    }
+    function _edit_image(data) {
+        var _data = data;
     }
     function _edit_file(data) {
         var _data = data;
