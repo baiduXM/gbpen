3bd82350c1827b0d0878a549f90899085208c914
BoomThanks <1195015834@qq.com>
2016-06-07 17:18:33 +0800

    统计

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -94,16 +94,16 @@ mainApp.config(function ($routeProvider, $httpProvider) {
                     $(this).parent().addClass('selected').siblings().removeClass('selected');
                 }
             });
-//            //===获取统计数据===
-//            var ahref = $(".selected>a").attr("href")
-//            if (ahref == "#/visit") {
-//                $.get('../statis-get', function (json) {
-//                    var data = json.data;
-//                    $(".cxb_list_n2>.cxb_list_m").html(data.count_today);//今日访问数量
-//                    $(".cxb_list_n3>.cxb_list_m").html(data.count_all);//总访客数量
-//                    $(".cxb_list_n4>.cxb_list_m").html(data.mobile_count);//移动端访问量
-//                });
-//            }
+            //===获取统计数据===
+            var ahref = $(".selected>a").attr("href")
+            if (ahref == "#/visit") {
+                $.get('../statis-get', function (json) {
+                    var data = json.data;
+                    $(".cxb_list_n2>.cxb_list_m").html(data.count_today);//今日访问数量
+                    $(".cxb_list_n3>.cxb_list_m").html(data.count_all);//总访客数量
+                    $(".cxb_list_n4>.cxb_list_m").html(data.mobile_count);//移动端访问量
+                });
+            }
             locathash ? $('#blob').animate({left: $('nav .nav a[href="' + locathash + '"]').parent().position().left}, 1000, 'easeOutExpo').addClass('rotation') : '';
         }, 200);
         /*
