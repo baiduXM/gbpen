f845ab87488f038ddc99e67924b29b9b3c922bdb
BoomThanks <1195015834@qq.com>
2016-06-14 09:45:46 +0800

    万用表单bug

--- public/admin/pages/js/addform.js
+++ public/admin/pages/js/addform.js
@@ -167,7 +167,7 @@ function addformController($scope, $http, $location) {
         var _data = data;
         var _div = '';
         $.each(_data, function (k, v) {
-            _div += '<li class="utility" data-id="' + v.id + '">\
+            _div += '<li class="utility" id="' + v.id + '">\
                         <span class="title">' + v.title + '</span>\
                         <i class="iconfont icon-liebiao grey tpl_info"></i>\
                     </li>';
@@ -185,7 +185,7 @@ function addformController($scope, $http, $location) {
      * @returns {undefined}
      */
     function bindElementEvent(_this) {
-        var element_id = _this.data('id');
+        var element_id = _this.attr('id');
         //===添加组件===
         $.post('../form-column-add', {form_id: form_id, element_id: element_id}, function (json) {
             var _data = json.data;
@@ -219,10 +219,10 @@ function addformController($scope, $http, $location) {
         if ($('li[data-id="' + data.column_id + '"').length > 0) {
             $('li[data-id="' + data.column_id + '"').html(_div);
         } else {
-            $('.add-show').append(_div_li);
+            $('.element-show').append(_div_li);
         }
         //===绑定元素点击响应事件===
-        $(".add-show>li").unbind('click').on('click', function () {
+        $(".element-show>li").unbind('click').on('click', function () {
             $('.tab-head-item').removeClass('tab-head-item-active');
             $('#item_2').addClass('tab-head-item-active');
             $('.tab-content-item').hide();
@@ -235,12 +235,11 @@ function addformController($scope, $http, $location) {
             }, 'json');
         });
         //===绑定元素鼠标滑动事件===
-        $(".add-show>li").unbind('hover').hover(function () {
+        $(".element-show>li").unbind('hover').hover(function () {
             $(this).addClass("hover");
         }, function () {
             $(this).removeClass("hover");
         });
-
     }
 
     function _show_text(data) {
@@ -405,8 +404,7 @@ function addformController($scope, $http, $location) {
         _div += '<span class="option-item"><input type="button" class="delete_column" value="删除"/></span>';
         _div += '</li>';
         $('#element-edit').html(_div);
-
-        $(".add-show>li").siblings().removeClass("click");
+        $(".element-show>li").siblings().removeClass("click");
         $('li[data-id="' + _data.column_id + '"]').addClass("click");
         //===加载操作===
         save_column(_data);
