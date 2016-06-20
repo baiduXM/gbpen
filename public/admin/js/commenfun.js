59d8779f8f7b3ad57e49ba6477559044af8ff851
BoomThanks <1195015834@qq.com>
2016-06-20 16:29:22 +0800

    4

--- public/admin/js/commenfun.js
+++ public/admin/js/commenfun.js
@@ -228,6 +228,8 @@ WarningBox.prototype = {
                 }
             };
             $image.cropper(options);
+            console.log(defaults);
+            console.log('===defaults===');
             this._UpFunction($image, defaults.ajaxurl, defaults.IsBaseShow, defaults.IsOneNatural, defaults.oncallback);
         } else {
             this._Schedule(defaults.oncallback);    // 带进度条
