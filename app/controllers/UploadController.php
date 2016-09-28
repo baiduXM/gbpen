39ff857ebcfa3289e0febd4de74a36ef185cc2e5
llyynnee <2279083016@qq.com>
2016-09-28 17:27:23 +0800

    批量上传图片

--- app/controllers/UploadController.php
+++ app/controllers/UploadController.php
@@ -187,48 +187,74 @@ class UploadController extends BaseController {
             $ftp_array[1] = isset($ftp_array[1]) ? $ftp_array[1] : $port;
             $conn = ftp_connect($ftp_array[0], $ftp_array[1]);
             $filesize = 0; //===文件大小===
-            foreach ((array) $files as $fileName) {
-                $filepath = public_path('customers/' . $customer . '/cache_images/' . $fileName);
-                if (file_exists($filepath)) {
-                    $filesize += filesize($filepath); //===累加大小
-                    $file = explode('.', $fileName);
-                    $type = end($file);
-                    $up_result = copy(public_path('customers/' . $customer . '/cache_images/' . $fileName), $destinationPath . '/l/' . $target . '/' . $fileName);
-                    if ($up_result) {
-                        $s_path = $destinationPath . '/s/' . $target . '/' . $fileName;
-                        $img_info = getimagesize($destinationPath . '/l/' . $target . '/' . $fileName);
-                        switch ($img_info[2]) {
-                            case 1:$type = 'gif';
-                                break;
-                            case 2:$type = 'jpg';
-                                break;
-                            case 3:$type = 'png';
-                                break;
-                        }
-                        $this->resizeImage($destinationPath . '/l/' . $target . '/' . $fileName, $type, $s_path, $img_size, $img_size);
-                        copy($destinationPath . '/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/' . $target . '/' . $fileName));
-                        $mobile_s_path = public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName;
-                        $this->resizeImage(public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, $type, $mobile_s_path, $img_size, $img_size);
-
-                        if ($conn) {
-                            ftp_login($conn, $customerinfo->ftp_user, $customerinfo->ftp_pwd);
-                            ftp_pasv($conn, 1);
-                            if (trim($ftp) == '1') {
-                                ftp_put($conn, $customer . '/images/l/' . $target . '/' . $fileName, $destinationPath . '/l/' . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $customer . '/images/s/' . $target . '/' . $fileName, $destinationPath . '/s/' . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $customer . '/mobile/images/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $customer . '/mobile/images/s/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName, FTP_BINARY);
-                            } else {
-                                ftp_put($conn, $ftpdir . '/' . 'images/l/' . $target . '/' . $fileName, $destinationPath . '/l/' . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $ftpdir . '/' . 'images/s/' . $target . '/' . $fileName, $destinationPath . '/s/' . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $ftpdir . '/' . 'mobile/images/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, FTP_BINARY);
-                                ftp_put($conn, $ftpdir . '/' . 'mobile/images/s/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName, FTP_BINARY);
+            if(file_exists(public_path('customers/' . $customer . '/img.zip'))){
+                @unlink(public_path('customers/' . $customer . '/img.zip'));
+            }
+            $zip = new ZipArchive;
+            if($zip->open(public_path('customers/' . $customer . '/img.zip'), ZipArchive::CREATE) === TRUE){
+                foreach ((array) $files as $fileName) {
+                    $filepath = public_path('customers/' . $customer . '/cache_images/' . $fileName);
+                    if (file_exists($filepath)) {
+                        $filesize += filesize($filepath); //===累加大小
+                        $file = explode('.', $fileName);
+                        $type = end($file);
+                        $up_result = copy(public_path('customers/' . $customer . '/cache_images/' . $fileName), $destinationPath . '/l/' . $target . '/' . $fileName);
+                        if ($up_result) {
+                            $s_path = $destinationPath . '/s/' . $target . '/' . $fileName;
+                            $img_info = getimagesize($destinationPath . '/l/' . $target . '/' . $fileName);
+                            switch ($img_info[2]) {
+                                case 1:$type = 'gif';
+                                    break;
+                                case 2:$type = 'jpg';
+                                    break;
+                                case 3:$type = 'png';
+                                    break;
                             }
+                            $this->resizeImage($destinationPath . '/l/' . $target . '/' . $fileName, $type, $s_path, $img_size, $img_size);
+                            copy($destinationPath . '/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/' . $target . '/' . $fileName));
+                            $mobile_s_path = public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName;
+                            $this->resizeImage(public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, $type, $mobile_s_path, $img_size, $img_size);
+                            if ($conn) {
+                                ftp_login($conn, $customerinfo->ftp_user, $customerinfo->ftp_pwd);
+                                ftp_pasv($conn, 1);
+                                if (trim($ftp) == '1') {
+                                    $zip->addFile($destinationPath . '/l/' . $target . '/' . $fileName,  'images/l/' . $target . '/' . $fileName);
+                                    $zip->addFile($destinationPath . '/s/' . $target . '/' . $fileName, 'images/s/' . $target . '/' . $fileName);
+//                                    $zip->addFile(public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName,'mobile/images/l/' . $target . '/' . $fileName);
+//                                    $zip->addFile(public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName,'mobile/images/s/' . $target . '/' . $fileName);
+//                                    ftp_put($conn, $customer . '/images/l/' . $target . '/' . $fileName, $destinationPath . '/l/' . $target . '/' . $fileName, FTP_BINARY);
+//                                    ftp_put($conn, $customer . '/images/s/' . $target . '/' . $fileName, $destinationPath . '/s/' . $target . '/' . $fileName, FTP_BINARY);
+//                                    ftp_put($conn, $customer . '/mobile/images/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, FTP_BINARY);
+//                                    ftp_put($conn, $customer . '/mobile/images/s/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName, FTP_BINARY);
+                                } else {
+                                    ftp_put($conn, $ftpdir . '/' . 'images/l/' . $target . '/' . $fileName, $destinationPath . '/l/' . $target . '/' . $fileName, FTP_BINARY);
+                                    ftp_put($conn, $ftpdir . '/' . 'images/s/' . $target . '/' . $fileName, $destinationPath . '/s/' . $target . '/' . $fileName, FTP_BINARY);
+                                    ftp_put($conn, $ftpdir . '/' . 'mobile/images/l/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/l/') . $target . '/' . $fileName, FTP_BINARY);
+                                    ftp_put($conn, $ftpdir . '/' . 'mobile/images/s/' . $target . '/' . $fileName, public_path('customers/' . $customer . '/mobile/images/s/') . $target . '/' . $fileName, FTP_BINARY);
+                                }
+                            }
+                            $data[$i]['name'] = $fileName;
+                            $data[$i]['url'] = asset('customers/' . $customer . '/images/l/' . $target . '/' . $fileName);
+                            $data[$i]['s_url'] = '/images/s/' . $target . '/' . $fileName;
+                            $i++;
                         }
-                        $data[$i]['name'] = $fileName;
-                        $data[$i]['url'] = asset('customers/' . $customer . '/images/l/' . $target . '/' . $fileName);
-                        $data[$i]['s_url'] = '/images/s/' . $target . '/' . $fileName;
-                        $i++;
+                    }
+                }
+                $zip->close();
+                if ($conn) {
+                    if (trim($ftp) == '1') {
+                        ftp_login($conn, $customerinfo->ftp_user, $customerinfo->ftp_pwd);
+                        ftp_pasv($conn, 1);
+                        $weburl = Customer::where('id', $cus_id)->pluck('weburl');
+                        $suf_url = str_replace('http://c', '', $weburl);
+                        $cus_name = strtolower(Customer::where('id', $cus_id)->pluck('name'));
+                        if (trim($ftp) == '1') {
+                            $ftp_pcdomain = "http://" . $cus_name . $suf_url;
+                        }
+                        ftp_put($conn, $customer . '/img.zip', public_path('customers/' . $customer . '/img.zip'), FTP_BINARY);
+                        ftp_put($conn, $customer . '/img_unzip.php', public_path('packages/img_unzip.php'), FTP_ASCII);
+                        @file_get_contents("$ftp_pcdomain/img_unzip.php");
+                        @unlink(public_path('customers/' . $customer . '/img.zip'));
                     }
                 }
             }
---
+++ public/packages/img_unzip.php
@@ -0,0 +1,18 @@
+<?php
+if(file_exists('img.zip')){
+        copy("img.zip",'mobile/img.zip');
+        $zip = new ZipArchive;//新建一个ZipArchive的对象
+	if ($zip->open('mobile/img.zip') === TRUE)
+	{
+	$zip->extractTo('./mobile/');//假设解压缩到在当前路径下images文件夹的子文件夹php
+	}
+	$zip->close();//关闭处理的zip文件
+	@unlink('./mobile/img.zip');
+	$zip = new ZipArchive;//新建一个ZipArchive的对象
+	if ($zip->open('img.zip') === TRUE)
+	{
+	$zip->extractTo('./');//假设解压缩到在当前路径下images文件夹的子文件夹php
+	}
+	$zip->close();//关闭处理的zip文件
+	@unlink('./img.zip');
+}
