117ba1599a11e78697d9f4010e7c19883736c976
Hsieh <1195015834@qq.com>
2016-12-27 17:31:19 +0800

    测试

--- public/admin/pages/js/setting.js
+++ public/admin/pages/js/setting.js
@@ -17,6 +17,21 @@ function settingController($scope, $http) {
             this._Addform();
             this._changeLang();
             this._changeTab();//选项卡切换
+            this._bilingual();//双语样式设置
+        },
+        _bilingual: function () {
+            var info = '<div class="quicklist-r inline-block">\
+                        <div class="ml5">\
+                            <ul class="fl consultation colorsetting">\n\
+                                <li class="consultation-item">\
+                                    <label class="message-name">pc主色：</label><div><input type="color" class="colors" value="' + this.ColorRet(colors['pc'][0]) + '" name="pc_mainColor" id="pc_mainColor" data-id="pc_mainColor"/><input type="text" class="pc_mainColor" value="' + this.ColorRet(colors['pc'][0]) + '" data-id="pc_mainColor" /></div>\
+                                    <label class="message-name">pc鼠标停留时颜色：</label><div><input type="color" class="colors" value="' + this.ColorRet(colors['pc'][1]) + '" name="pc_secondColor" id="pc_secondColor" data-id="pc_secondColor"/><input type="text" class="pc_secondColor" value="' + this.ColorRet(colors['pc'][1]) + '" data-id="pc_secondColor"/></div>\
+                                    <label class="message-name">pc图标颜色：</label><div><input type="color" class="colors" value="' + this.ColorRet(colors['pc'][2]) + '" name="pc_textColor" id="pc_textColor" data-id="pc_textColor"/><input type="text" class="pc_textColor" value="' + this.ColorRet(colors['pc'][2]) + '" data-id="pc_textColor" /></div>\
+				</li>\n\
+                            </ul>\
+                            <button class="colorsclear btn btn-default" >还原</button>\
+			</div>\n\
+                    </div>';
         },
         _changeTab: function () {
             $(".setting-name").click(function (e) {
