28c19c74dea511c61bf24b007f41d4c3b106678f
Hsieh <1195015834@qq.com>
2016-12-27 10:39:37 +0800

    API用户密码

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -134,8 +134,11 @@ class ApiController extends BaseController {
                 }
             } else {
                 //增加操作
-                $update['password'] = isset(Input::get("password"))? Hash::make(Input::get("password")):Hash::make($update['name']);
-//                $update['password'] = Hash::make($update['name']);
+                if (isset(Input::get("password"))) {
+                    $update['password'] = Hash::make(Input::get("password"));
+                } else {
+                    $update['password'] = Hash::make($update['name']);
+                }
                 $insert_id = Customer::insertGetId($update);
                 if ($insert_id) {
                     $pc_id = Template::where('tpl_num', $update['pc_tpl_num'])->where('type', 1)->pluck('id');
