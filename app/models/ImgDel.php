d144cabb28eb3ae9a772a93878438e3ee6935848
kousiro <1223746977@qq.com>
2017-03-13 15:37:48 +0800

    测试1.4

--- app/models/ImgDel.php
+++ app/models/ImgDel.php
@@ -11,19 +11,18 @@ class ImgDel extends Eloquent {
             //===释放用户空间容量===
             $customer = Auth::user()->name;
             $filepath = public_path('customers/' . $customer . '/images/l/' . $target . '/' . $img);
-            $uepath = public_path('customers/' . $customer . '/images/ueditor/' . $img);
+            // $uepath = public_path('customers/' . $customer . '/images/ueditor/' . $img);
             if (is_file($filepath)) {
                 file_put_contents("test.txt", "第二".$filepath,FILE_APPEND);
                 $size = filesize($filepath); //===images/l不一定有，要推送后才有图片
             } 
-            elseif(is_file($uepath)){
-                file_put_contents("test.txt", "第三".$uepath,FILE_APPEND);
-                $size = filesize($uepath);
-            }
+            // elseif(is_file($uepath)){
+            //     file_put_contents("test.txt", "第三".$uepath,FILE_APPEND);
+            //     $size = filesize($uepath);
+            // }
             else {
                 $size = 0;
             }
-            file_put_contents("size.txt", $size);
             $Capacity = new CapacityController;
             $Capacity->change_capa($size, 'free');
             //===end===
