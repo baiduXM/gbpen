f255608ace77d418c29b9ecdb2c76ffba82939c0
kousiro <1223746977@qq.com>
2017-03-14 07:57:44 +0800

    测试2.9

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -304,7 +304,7 @@ class ArticleController extends BaseController {
             if ($article->img != '') {
                 $img = array();
                 $img[] = asset("customers/$customer/images/l/articles") . '/' . $article->img;
-                $moreimg = Moreimg::where('a_id', $id)->lists('img');
+                $moreimg = Moreimg::where('a_id', $id)->where('from','!==','ueditor')->lists('img');
                 if (count($moreimg)) {
                     foreach ($moreimg as $v) {
                         $img[] = asset("customers/$customer/images/l/articles") . '/' . $v;
