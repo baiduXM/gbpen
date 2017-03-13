35b4ecb8acd5976b70c4fce6c86077f6e1d66e34
kousiro <1223746977@qq.com>
2017-03-13 15:48:07 +0800

    测试1.4

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -76,8 +76,9 @@ class ArticleController extends BaseController {
         $article->pushed = 1;
         //===ueditor文件统计容量===
         $Capacity = new CapacityController();
-        $Capacity->compare_filename($article->content, $article->file_array);
         $article->file_array = $Capacity->reg_ueditor_content($article->content);
+        $Capacity->compare_filename($article->content, $article->file_array);
+        
         $ue_img=explode(",", $article->file_array);
         //===end===
         $result = $article->save();
