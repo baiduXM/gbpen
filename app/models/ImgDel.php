f29fb2c6eae5e98984df10a69436b46f6317b30a
kousiro <1223746977@qq.com>
2017-03-14 11:10:40 +0800

    测试3.3

--- app/models/ImgDel.php
+++ app/models/ImgDel.php
@@ -15,29 +15,7 @@ class ImgDel extends Eloquent {
             if (is_file($filepath)) {
                 $size = filesize($filepath); //===images/l不一定有，要推送后才有图片
             } 
-            // elseif(is_file($uepath)){
-            //     if($from == ""){
-            //       $size = filesize($uepath);  
-            //     }                
-            // }
-            else {
-                $size = 0;
-            }
-            $Capacity = new CapacityController;
-            $Capacity->change_capa($size, 'free');
-            //===end===
-            return $Capacity->format_bytes($size);
-//            return DB::table($this->table)->insert(array('img' => $img, 'cus_id' => Auth::id(), 'target' => $target));
-        }
-    }
-    public function myusave($img) {
-        if ($img != '') {
-            DB::table($this->table)->insert(array('img' => $img, 'cus_id' => Auth::id(), 'target' => $target));
-            //===释放用户空间容量===
-            $customer = Auth::user()->name;
-            $uepath = public_path('customers/' . $customer . '/images/ueditor/' . $img);
-
-            if(is_file($uepath)){
+            elseif(is_file($uepath)){
                 if($from == ""){
                   $size = filesize($uepath);  
                 }                
@@ -49,6 +27,7 @@ class ImgDel extends Eloquent {
             $Capacity->change_capa($size, 'free');
             //===end===
             return $Capacity->format_bytes($size);
+//            return DB::table($this->table)->insert(array('img' => $img, 'cus_id' => Auth::id(), 'target' => $target));
         }
     }
 
