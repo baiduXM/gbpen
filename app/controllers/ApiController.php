92757e7d3797f41cb730ea40c638a1ed65461879
kousiro <1223746977@qq.com>
2017-04-25 18:24:15 +0800

    外域名独立1.0

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -100,6 +100,10 @@ class ApiController extends BaseController
             $update['ftp_user_b'] = trim(Input::get('ftp_user_b'));
             $update['ftp_pwd_b'] = trim(Input::get('ftp_pwd_b'));
 
+            //获取外域PC，手机域名
+            $update['pc_out_domain'] = trim(Input::get('pc_out_domain'));
+            $update['mobile_out_domain'] = trim(Input::get('mobile_out_domain'));
+
             //===绑定账户===
             $switch_cus_name = Input::get('switch_cus_name');
             if (!empty($switch_cus_name)) {
