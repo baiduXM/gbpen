f5f0eaae6799c6ccb0f33c8992b036de00bc80cf
kousiro <1223746977@qq.com>
2017-06-10 09:09:09 +0800

    Revert "头部js调用访问量计数php"
    This reverts commit 9df8d10bcddab4a8a5503e41969196ddac37a520.

--- app/controllers/PrintController.php
+++ app/controllers/PrintController.php
@@ -1061,7 +1061,6 @@ class PrintController extends BaseController
                 curl_close($ch);
             }
             $headscript = $customer_info->pc_header_script;
-            $headscript .= '<script type="text/javascript" src="../visits.php"></script>';
             //===版权选择===
             switch ($customer_info->copyright) {
                 case 'en_xiamen':
@@ -1156,7 +1155,6 @@ class PrintController extends BaseController
                 $add_color_css = '<script type="text/javascript">$("head").append(\'<link rel="stylesheet" href="' . $css_file_name . '" type="text/css">\')</script>';
             }
             $headscript = $customer_info->mobile_header_script;
-            $headscript .= '<script type="text/javascript" src="../../visits.php"></script>';
             $footprint = $customer_info->mobile_footer;
             $footscript = $customer_info->mobile_footer_script;
 //            $footscript .= $formJS;
