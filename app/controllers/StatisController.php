c00d1b1c40957f8c7faa1e93fafb8b276692b1cc
kousiro <1223746977@qq.com>
2017-07-11 12:23:19 +0800

    获取访问量

--- app/controllers/StatisController.php
+++ app/controllers/StatisController.php
@@ -21,6 +21,38 @@ class StatisController extends BaseController {
     //     return $res;
     // }
 
+    public function getCount() {
+        //获取用户信息
+        $cus_id = Auth::id();
+        $customer = Customer::where('id',$cus_id)->get()->toArray();
+
+        $address = $customer[0]['ftp_address'];
+        $name = $customer[0]['name'];
+        @$res = file_get_contents('http://'.$address.'/'.$name.'/read.php');
+        $data = json_decode($res,true);
+
+        //如果有B服务器
+        if($customer[0]['ftp_address_b']){
+            $address_b = $customer[0]['ftp_address_b'];
+            @$res_b = file_get_contents('http://'.$address_b.'/'.$name.'/read.php');
+            $data_b = json_decode($res_b,true);
+        }else{
+            $data_b =array();
+        }
+        
+        if($data != NULL && $data_b != NULL){
+            $end['count_all'] = $data[0]['count_all'] + $data_b[0]['count_all'];
+            $end['count_mobile'] = $data[0]['count_mobile'] + $data_b[0]['count_mobile'];
+            $end['count_today'] = $data[0]['count_today'] + $data_b[0]['count_today'];
+            $res = Response::json(['err' => 0, 'msg' => '获取统计数据成功', 'data' => $end]);
+        }elseif ($data != NULL) {
+            $res = Response::json(['err' => 0, 'msg' => '获取统计数据成功', 'data' => $data[0]]);
+        } else {
+            $res = Response::json(['err' => 1, 'msg' => '获取统计数据失败', 'data' => null]);
+        }
+        return $res;
+    }
+
 }
 
 ?>
