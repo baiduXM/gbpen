365156e5b89ee0dd052381bec31483ccdc612db2
kousiro <1223746977@qq.com>
2017-07-17 14:19:49 +0800

    文章页码跳转隐藏分类筛选恢复

--- public/admin/pages/js/article.js
+++ public/admin/pages/js/article.js
@@ -125,7 +125,7 @@ function articleController($scope, $http, $location) {
                         <th>操作</th>\n\
                     </tr>\n\
                     <tr class="sapces"></tr>';
-                    var now_page = getUrlParam('p') ? getUrlParam('p') : 1;
+                    // var now_page = getUrlParam('p') ? getUrlParam('p') : 1;
                     var search_word = getUrlParam('search_word') ? getUrlParam('search_word') : '';
                     $.each(article_d, function (k, v) {
                         _div += '<tr class="article-check">\n\
@@ -143,7 +143,7 @@ function articleController($scope, $http, $location) {
                                         </td>\n\
                                         <td>' + v.created_at + '</td>\n\\n\
                                         <td><input class="sort" type="text" data-id="' + v.id + '"  value="' + ((v.sort == 1000000) ? '' : v.sort) + '" /></td>\n\
-                                        <td><a style="margin:0 10px;" class="column-edit pr" href="#/addarticle?id=' + v.id + '&c_id=' + v.c_id + '&p=' + now_page + '&sw='+search_word+'"><i class="fa iconfont icon-bianji"></i><div class="warning"><i class="iconfont' + (v.img_err ? ' icon-gantanhao' : '') + '"></i></div></a><a class="delv" name="' + v.id + '"><i class="fa iconfont icon-delete mr5"></i></a></td>\n\
+                                        <td><a style="margin:0 10px;" class="column-edit pr" href="#/addarticle?id=' + v.id + '&c_id=' + v.c_id + '&p=' + page + '&sw='+search_word+'"><i class="fa iconfont icon-bianji"></i><div class="warning"><i class="iconfont' + (v.img_err ? ' icon-gantanhao' : '') + '"></i></div></a><a class="delv" name="' + v.id + '"><i class="fa iconfont icon-delete mr5"></i></a></td>\n\
                                     </tr>';
                     });
                     $('.a-table').html(_div);
@@ -173,24 +173,27 @@ function articleController($scope, $http, $location) {
 //                                urlparam += '&p=' + (page_index + 1);
 //                            }
                             page_index = page_index + 1;
-                            // $scope.getArticleList({
-                            //     first: false,
-                            //     // page: page_index + 1,
-                            //     page: page_index,
-                            //     cat_id: cat_id,
-                            //     is_star: is_star,
-                            //     ser_name: ser_name,
-                            //     search_word: search_word,
-                            //     ser_active: ser_active,
-                            // });
+                            $scope.getArticleList({
+                                first: false,
+                                // page: page_index + 1,
+                                page: page_index,
+                                cat_id: cat_id,
+                                is_star: is_star,
+                                ser_name: ser_name,
+                                search_word: search_word,
+                                ser_active: ser_active,
+                            });
 
-                            var url = '#/article?p=' + page_index;
-                            var search_word = getUrlParam('search_word');
-                            if(search_word){
-                                url+= '&search_word=' + search_word;
-                            }
+                            // var url = '#/article?p=' + page_index;
+                            // var search_word = getUrlParam('search_word');
+                            // if(search_word){
+                            //     url+= '&search_word=' + search_word;
+                            // }
+                            // if(cat_id){
+                            //     url+= '&id=' + cat_id;
+                            // }
 
-                            window.location.hash = url;
+                            // window.location.hash = url;
                         }
                     });
                 } else {
@@ -248,6 +251,23 @@ function articleController($scope, $http, $location) {
                     $(this).parent("div").find(".sap_tit").show();
                     $(this).hide();
                 });
+                //页码跳转
+                // $("#cpage").click(function(){
+                //     var tpage = $("#tpage").val();
+                //     if(tpage==null){
+                //         tpage = 1;
+                //     }
+                //     $scope.getArticleList({
+                //         first: false,
+                //         page: tpage,
+                //         cat_id: cat_id,
+                //         is_star: is_star,
+                //         ser_name: ser_name,
+                //         search_word: search_word,
+                //         ser_active: ser_active,
+                //     });
+                // });
+
             }); // checkJSON结束
         });
     };
