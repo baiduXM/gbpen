f98e2c6693ce5708dfb26ea55ff24ed6bb8c19c1
kousiro <1223746977@qq.com>
2017-10-24 12:48:48 +0800

    公告和申请

---
+++ app/controllers/SendController.php
@@ -0,0 +1,144 @@
+<?php
+
+class SendController extends BaseController
+{
+    //图片数组
+    protected $imageArr = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'svg'];
+    //用户ID
+    protected $cus_id;
+    //用户名
+    protected $customer;    
+
+    //G名片申请
+    public function weicardApply(){
+
+        //提交的数据
+        $data['company'] = Input::get('txtCompanyName');
+        $data['name'] = Input::get('txtName');
+        $data['phone'] = Input::get('txtContact');
+        $data['city'] = Input::get('txtCity');
+        $data['email'] = Input::get('txtContent');
+        $data['num'] = Input::get('select3');
+
+        //数据验证
+        if(!$data['company']){
+            $result = ['err' => 1008, 'msg' => '公司名不能为空'];
+            return Response::json($result);
+        }
+        if(!$data['name']){
+            $result = ['err' => 1008, 'msg' => '姓名不能为空'];
+            return Response::json($result);
+        }
+        if(!is_numeric($data['phone'])){
+            $result = ['err' => 1008, 'msg' => '电话格式错误'];
+            return Response::json($result);
+        }
+        if(!preg_match("/^([0-9A-Za-z\\-_\\.]+)@([0-9a-z]+\\.[a-z]{2,3}(\\.[a-z]{2})?)$/i",$data['email'])){
+            $result = ['err' => 1008, 'msg' => '邮箱格式错误'];
+            return Response::json($result);
+        }
+
+        //其他数据
+        $this->cus_id = Auth::id();
+        $this->customer = Auth::user()->name;
+        $data['cus_id'] = $this->cus_id;
+        $data['cus_name'] = $this->customer;
+        $data['date'] = date('Y-m-d H:i:s',time());
+        
+        //上传文件处理
+        if(Input::hasFile('image')){
+            if (Input::file('image')->isValid())
+            {
+                $image = Input::file('image');
+                $fileRes = $this -> saveImg($image);
+                if($fileRes['err'] == 1000){
+                    $data['image'] = $fileRes['msg'];
+                } else {
+                    $result = ['err' => $fileRes['err'], 'msg' => $fileRes['msg']];
+                    return Response::json($result);
+                }
+            } else {
+                $result = ['err' => 1003, 'msg' => '无效文件'];
+                return Response::json($result);
+            }           
+        } else {
+            $result = ['err' => 1002, 'msg' => '文件上传失败'];
+            return Response::json($result);
+        }
+
+        //插入数据库
+        $id = GApply::insertGetId($data);
+        if($id){
+            //提交数据到G宝盆官网
+            $send = $this -> sendtoGbpen($data);
+            if($send['err'] == 1000){
+                $result = ['err' => 1000, 'msg' => '提交成功'];
+            }else{
+                $res = GApply::where('id' , $id)->update(['status' => 0]);
+                $result = ['err' => $send['err'], 'msg' => $send['msg']];
+            }
+        }else{
+            $result = ['err' => 1001, 'msg' => '写入失败'];
+        }        
+
+        return Response::json($result);
+        
+    }
+
+    //保存图片
+    public function saveImg($image){
+        //目标路径
+        $path = public_path('customers/'. $this->customer. '/images/other');
+        if(!is_dir($path)){
+            @mkdir($path);
+        }        
+        $extension = strtolower($image->getClientOriginalExtension());//拓展名
+        if(in_array($extension, $this->imageArr)) {
+            $fileName = time(). str_random(4). '.'. $extension;//文件名      
+            if($image->move($path, $fileName)){
+                $result = ['err' => 1000, 'msg' => $fileName];
+            } else {
+                $result = ['err' => 1004, 'msg' => '文件移动失败'];
+            }
+        } else {
+            $result = ['err' => 1005, 'msg' => '格式不正确'];
+        }        
+        
+        return $result;
+    }
+
+    //提交数据到G宝盆官网
+    protected function sendtoGbpen($data){
+        //目标路径
+        $destinationPath = './webroot/uploads/G_leaveword/'. $data['image'];
+        //判断文件是否存在
+        $localFile = public_path('customers/'. $this->customer. '/images/other/'. $data['image']);
+        if(file_exists($localFile)){
+            //上传图片到G宝盆官网
+            $conn = ftp_connect(GBPEN_FTP , GBPEN_FTP_PORT);
+            if($conn){
+                ftp_login($conn, GBPEN_FTP_USER, GBPEN_FTP_PASSWORD);
+                ftp_pasv($conn, 1);
+                ftp_put($conn, $destinationPath, $localFile, FTP_BINARY);
+                ftp_close($conn);
+            }
+        } else {
+            $result = ['err' => 1006, 'msg' => '文件不存在'];
+            return $result;
+        }
+
+        //发送数据
+        $common = new CommonController();
+        $res = $common->postsend(GBPEN_DOMAIN . "/leaveword4.php", $data);
+        $res = json_decode($res,true);
+        if($res['err'] == 1000){
+            $result = ['err' => 1000, 'msg' => 'G宝盆官网同步成功'];
+        } else {
+            $result = ['err' => $res['err'], 'msg' => $res['msg']];
+        }
+
+        return $result;
+
+    }
+
+}
--- app/controllers/StatisController.php
+++ app/controllers/StatisController.php
@@ -56,18 +56,44 @@ class StatisController extends BaseController {
     }
 
     public function customersNum(){
+        //客户数(未过滤已删除)
         $n01 = Customer::where('ftp_address','172.16.0.4')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->count();
         $n02 = Customer::where('ftp_address','172.16.0.18')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->count();
         $n03 = Customer::where('ftp_address','172.16.0.24')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->count();
         $hk01 = Customer::where('ftp_address','182.61.100.142')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->count();
-
+        //客户数(过滤已删除)
         $n1 = Customer::where('ftp_address','172.16.0.4')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->count();
         $n2 = Customer::where('ftp_address','172.16.0.18')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->count();
         $n3 = Customer::where('ftp_address','172.16.0.24')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->count();
         $hk1 = Customer::where('ftp_address','182.61.100.142')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->count();
+        //双站(未过滤已删除)
+        $s01 = Customer::where('ftp_address','172.16.0.4')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->whereIn('stage',array(3,4))->count();
+        $s02 = Customer::where('ftp_address','172.16.0.18')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->whereIn('stage',array(3,4))->count();
+        $s03 = Customer::where('ftp_address','172.16.0.24')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->whereIn('stage',array(3,4))->count();
+        $hks1 = Customer::where('ftp_address','182.61.100.142')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->whereIn('stage',array(3,4))->count();
+        //双站(过滤已删除)
+        $s1 = Customer::where('ftp_address','172.16.0.4')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->whereIn('stage',array(3,4))->count();
+        $s2 = Customer::where('ftp_address','172.16.0.18')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->whereIn('stage',array(3,4))->count();
+        $s3 = Customer::where('ftp_address','172.16.0.24')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->whereIn('stage',array(3,4))->count();
+        $hks = Customer::where('ftp_address','182.61.100.142')->where('name','not like','GP%')->where('name','not like','GM%')->where('name','not like','GT%')->where('name','not like','GG%')->where('name','not like','%PCN%')->where('name','not like','%MCN%')->where('name','not like','%PEN%')->where('name','not like','%MEN%')->where('is_del',1)->whereIn('stage',array(3,4))->count();
 
-        $count = '用户数量(包括已删除)：<br/>n01:'.$n01.'<br/>n02:'.$n02.'<br/>n03:'.$n03.'<br/>hk01:'.$hk01.'<hr/>过滤已删除用户后：<br>n01:'.$n1.'<br/>n02:'.$n2.'<br/>n03:'.$n3.'<br/>hk01:'.$hk1;
+        $count = '用户数量(包括已删除)：<br/>n01:'.$n01.'<br/>n02:'.$n02.'<br/>n03:'.$n03.'<br/>hk01:'.$hk01.'<hr/>过滤已删除用户后：<br>n01:'.$n1.'<br/>n02:'.$n2.'<br/>n03:'.$n3.'<br/>hk01:'.$hk1.'<hr/>
+            双站的用户(包括已删除)：<br>n01:'.$s01.'<br/>n02:'.$s02.'<br/>n03:'.$s03.'<br/>hk01:'.$hks1.'<hr/>
+            双站的用户：<br>n01:'.$s1.'<br/>n02:'.$s2.'<br/>n03:'.$s3.'<br/>hk01:'.$hks.'<hr/>';
         return $count;
+    }
+
+    /**
+     * 公告获取
+     */
+    public function getNotice(){
+        $notice = Notice::where('type',0)->where('is_on',1)->first();
+        if($notice){
+            $result = ['err' => 1000, 'msg' => $notice];
+        } else {
+            $result = ['err' => 1001, 'msg' => '无公告'];
+        }
+        return Response::json($result);
     }
 
 }
